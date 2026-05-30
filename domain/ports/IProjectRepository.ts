3220e838c17f79927874ffc5c0349daaa279787d
TomBertomeu <contact@tom-bertomeu.com>
2026-05-31 00:14:13 +0200

    refactor: drop findById from project repository port

--- domain/ports/IProjectRepository.ts
+++ domain/ports/IProjectRepository.ts
@@ -3,5 +3,4 @@ import type { Project } from "@/types/project";
 
 export interface IProjectRepository {
   findAll(lang: Language): Project[];
-  findById(id: string, lang: Language): Project | undefined;
 }
--- repositories/projectRepository.ts
+++ repositories/projectRepository.ts
@@ -24,10 +24,6 @@ class StaticProjectRepository implements IProjectRepository {
     return projectsData.map((p) => localizeProject(p, lang));
   }
 
-  findById(id: string, lang: Language): Project | undefined {
-    const project = projectsData.find((p) => p.id === id);
-    return project ? localizeProject(project, lang) : undefined;
-  }
 }
 
 export const projectRepository: IProjectRepository = new StaticProjectRepository();
